dbb761a0ac81e94a0592be3c99d4ad2a911f91d3
Ionut Cicio <ionutcicio.dev@gmail.com>
2023-02-28 21:44:06 +0100

    removed online store

--- docs/_app/immutable/chunks/2-f853bc61.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-d8ff1a51.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,26 +5,26 @@
         store: boolean = false;
 </script>
 
-<div class="relative z-50">
-    <div id="my-store-85014551" />
-    <div>
-        <script
-            data-cfasync="false"
-            type="text/javascript"
-            src="https://app.ecwid.com/script.js?85014551&data_platform=code&data_date=2023-02-28"
-            charset="utf-8"
-        ></script>
-        <script type="text/javascript">
-            xProductBrowser(
-                "categoriesPerRow=3",
-                "views=grid(20,3) list(60) table(60)",
-                "categoryView=grid",
-                "searchView=list",
-                "id=my-store-85014551"
-            );
-        </script>
-    </div>
-</div>
+<!-- <div class="relative z-50"> -->
+<!--     <div id="my-store-85014551" /> -->
+<!--     <div> -->
+<!--         <script -->
+<!--             data-cfasync="false" -->
+<!--             type="text/javascript" -->
+<!--             src="https://app.ecwid.com/script.js?85014551&data_platform=code&data_date=2023-02-28" -->
+<!--             charset="utf-8" -->
+<!--         ></script> -->
+<!--         <script type="text/javascript"> -->
+<!--             xProductBrowser( -->
+<!--                 "categoriesPerRow=3", -->
+<!--                 "views=grid(20,3) list(60) table(60)", -->
+<!--                 "categoryView=grid", -->
+<!--                 "searchView=list", -->
+<!--                 "id=my-store-85014551" -->
+<!--             ); -->
+<!--         </script> -->
+<!--     </div> -->
+<!-- </div> -->
 {#if store}
     <Store />
 {:else if home}
@@ -46,9 +46,9 @@
                 >
                     <img src="resume.svg" alt="Resume" class="w-14" />
                 </a>
-                <button on:click={() => (store = true)}>
-                    <img src="icon.svg" alt="Store" class="w-14" />
-                </button>
+                <!-- <button on:click={() => (store = true)}> -->
+                <!--     <img src="icon.svg" alt="Store" class="w-14" /> -->
+                <!-- </button> -->
             </div>
             <button on:click={() => (home = false)}>
                 <span
